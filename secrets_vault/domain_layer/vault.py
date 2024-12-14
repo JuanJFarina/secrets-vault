42fc3eacd4aeb28679157fc7a4336421a4b7b75d
Juan José Farina <juanjosefarina.jjf@gmail.com>
2024-12-14 03:46:56 -0300

    fix(config): rename vault method

--- secrets_vault/domain_layer/vault.py
+++ secrets_vault/domain_layer/vault.py
@@ -12,7 +12,7 @@ class Vault(BaseModel):
     def add_vault(self, path: str, secret: str | None = None) -> None:
         self.secrets_vaults[path] = SecretsVault(path=VaultPath(path=path))
         if secret:
-            self.add_secret_to_vault(path, secret)
+            self.modify_vault_secret(path, secret)
 
-    def add_secret_to_vault(self, path: str, value: str) -> None:
+    def modify_vault_secret(self, path: str, value: str) -> None:
         self.secrets_vaults[path].modify_secret(value)
